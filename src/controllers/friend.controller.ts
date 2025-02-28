dedf43742bedd0722c332ca089e8d83faa579b0c
Samiul Karim <samiulkarimprodhan@gmail.com>
2025-02-28 08:43:22 +0600

    refactor handleSendFriendRequest to simplify senderId retrieval and improve error handling

--- src/controllers/friend.controller.ts
+++ src/controllers/friend.controller.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import { Types } from 'mongoose';
 import { z } from 'zod';
 import UserModel from '../models/user.model';
 import * as socketStore from '../socket/socketStore';
@@ -56,25 +55,25 @@ export async function handleSearchFriend(req: Request, res: Response) {
 export async function handleSendFriendRequest(req: Request, res: Response) {
   try {
     // validate input
-    const { senderId, recipientId } = req.body;
-    const senderObjectId = new Types.ObjectId(senderId as string);
-    const recipientObjectId = new Types.ObjectId(recipientId as string);
+    const { recipientId } = req.body;
+    const senderId = req.user!._id;
 
     const recipient = await UserModel.findById(recipientId);
+    const sender = await UserModel.findById(senderId);
     if (!recipient) {
       throw new ApiError(404, 'Recipient not found');
     }
     // Check if recipient has already received a friend request from sender
 
-    if (!recipient?.friendRequests.includes(senderObjectId)) {
-      recipient?.friendRequests.push(senderObjectId);
+    if (!recipient?.friendRequests.includes(senderId)) {
+      recipient?.friendRequests.push(senderId);
       await recipient?.save();
     }
 
     // Emit real-time notification to the recipient
-    const sender = await UserModel.findById(senderObjectId);
-    socketStore.emitToUser(recipientObjectId.toString(), 'newFriendRequest', {
-      senderId: senderObjectId,
+
+    socketStore.emitToUser(recipientId, 'newFriendRequest', {
+      senderId: senderId,
       senderName: sender?.username,
       createdAt: new Date(),
     });
@@ -82,13 +81,6 @@ export async function handleSendFriendRequest(req: Request, res: Response) {
     // Send success response
     res.status(200).json({ message: 'Friend request sent successfully' });
   } catch (error) {
-    if (error instanceof z.ZodError) {
-      const firstError = error.errors[0];
-      return res.status(422).json({
-        message: firstError.message,
-        field: firstError.path.join('.'),
-      });
-    }
     handleApiError(res, error);
   }
 }
