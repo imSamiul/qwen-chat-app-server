53ed547ee908971b84c83633465e3781329c97ef
Samiul Karim <samiulkarimprodhan@gmail.com>
2025-03-19 18:08:10 +0600

    add notification read functionality and update routes for marking notifications as read

--- src/controllers/notification.controller.ts
+++ src/controllers/notification.controller.ts
@@ -2,13 +2,33 @@ import { Request, Response } from 'express';
 import NotificationModel from '../models/notification.model';
 import { handleApiError } from '../utils/apiError';
 
+// get all notifications for a user
 export async function handleGetNotifications(req: Request, res: Response) {
   try {
     const userId = req.user!._id;
     const notifications = await NotificationModel.find({
       recipient: userId,
-    }).sort({ createdAt: -1 });
-    res.json(notifications);
+    }).sort({ createdAt: -1, isRead: 1 });
+    res.status(200).json(notifications);
+  } catch (error) {
+    handleApiError(res, error);
+  }
+}
+
+// mark all the notification as read for a user
+export async function handleMarkNotificationRead(req: Request, res: Response) {
+  try {
+    const userId = req.user!._id;
+
+    await NotificationModel.updateMany(
+      {
+        recipient: userId,
+        isRead: false,
+      },
+      { isRead: true }
+    );
+
+    res.status(200).json({ message: 'Notifications marked as read' });
   } catch (error) {
     handleApiError(res, error);
   }
--- src/routes/notification.routes.ts
+++ src/routes/notification.routes.ts
@@ -1,8 +1,17 @@
 import express from 'express';
-import { handleGetNotifications } from '../controllers/notification.controller';
+import {
+  handleGetNotifications,
+  handleMarkNotificationRead,
+} from '../controllers/notification.controller';
 import { isAuthenticated } from '../middleware/isAuthenticated';
 const router = express.Router();
 
 router.get('/get-notifications', isAuthenticated, handleGetNotifications);
+
+router.patch(
+  '/mark-notifications-read',
+  isAuthenticated,
+  handleMarkNotificationRead
+);
 
 export default router;
